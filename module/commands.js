7dc295136b6a7980267331b347f05d44018a66e8
ShibuyaBuya <121205226+ShibuyaBuya@users.noreply.github.com>
2023-07-16 21:15:44 +0700

    Create commands.js

---
+++ module/commands.js
@@ -0,0 +1,20 @@
+module.exports.commands = function (msg, client, clientMsg) {
+    var result;
+
+    if (msg[1] == "menu") {
+        result = `` `
+# *MENU*
+@ tui/menu - Menu
+@ tui/whoami - Whoami, usage: tui/whoami
+@ tui/set - To set something for documentation write tui/set -Doc
+        ` ``;
+
+
+    }else{
+        result = ```
+$msg: Maybe the command is not added or maintenance
+        ```
+    }
+    clientMsg.reply(result);
+
+}
