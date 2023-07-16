5b4ada8c67154d26efb909212ed063455f2425af
ShibuyaBuya <121205226+ShibuyaBuya@users.noreply.github.com>
2023-07-17 00:17:56 +0700

    Update commands.js

--- module/commands.js
+++ module/commands.js
@@ -1,12 +1,18 @@
 const os = require('os');
+const fs = require('fs');
+const path = require('path');
 
+// Mendapatkan PWD (working directory)
+const pwd = path.resolve(__dirname);
 const platform = os.platform();
 const release = os.release();
 module.exports.commands = function (msg, client, clientMsg) {
     var result;
-
+    const modul = `./${msg[1]}.js`;
     if (msg[1] == "menu") {
         result = `
+# *PWD*
+@ ${pwd}
 # *MENU*
 @ tui/menu - Menu
 @ tui/whoami - Whoami, usage: tui/whoami
@@ -15,17 +21,21 @@ module.exports.commands = function (msg, client, clientMsg) {
 
 
     }else if (msg[1] == "whoami"){
-        result = `
+	result = `
 # *WHOAMI*
 Platform ${platform}
 Release ${release}
 _CentOS_
 `;
     }else{
-        result = `
-${msg}: Maybe the command is not added or maintenance
-        `;
+	if (fs.existsSync(`./module/${msg[1]}.js`)){
+	    const { run } = require(modul);
+	    result = run(msg, client, clientMsg);
+	}else{
+            result = `
+${msg}: Maybe the command is not added or maintenance, Thank you :)
+            `;
+	}
     }
     clientMsg.reply(result);
-
 }
