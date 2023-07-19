2109e5b14d2a805f67346da4a154ef0f214c338a
ShibuyaBuya <121205226+ShibuyaBuya@users.noreply.github.com>
2023-07-19 16:21:36 +0700

    Update example.js

--- module/example.js
+++ module/example.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 module.exports.PERMISSION = function (){
     return "EXAMPLE::23; SYSTEM_MODULE; _IMPORTANT; *;";
 }
@@ -9,15 +9,4 @@ This is an example ->
 *${msg}*
 *${client}*
 *${cmsg}*
-=======
-module.exports.run = function (msg, client, cmsg){
-    return `
-This is an example ->
-
-*${msg}*
-*${client}*
-*${cMsg}*
->>>>>>> bbf623001888e193dd82e0d0158b2941bb972317
-
-`;
 }
