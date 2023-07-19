2ba1c412eda0a5d0f8380b2f55700e880857dd7e
ShibuyaBuya <121205226+ShibuyaBuya@users.noreply.github.com>
2023-07-19 16:22:27 +0700

    Update ping.js

--- module/ping.js
+++ module/ping.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 module.exports.PERMISSION = function (){
     return "PING::23; SYSTEM_MODULE; _IMPORTANT; *;";
 }
-=======
->>>>>>> bbf623001888e193dd82e0d0158b2941bb972317
 module.exports.run = function (msg, client, cmsg){
     cmsg.reply("_Processing!_");
     return "Pong -> *Responded*";
