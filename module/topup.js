a40df37fd5b1f7640ff9c466cfff6a269734a15d
ShibuyaBuya <121205226+ShibuyaBuya@users.noreply.github.com>
2023-07-23 18:40:31 +0000

    update topup

--- module/topup.js
+++ module/topup.js
@@ -9,7 +9,8 @@ module.exports.run = async (msg, client, cmsg) => {
         cmsg.reply("Usage: !topup/<amoint>/<number> (optional)");
         return;
     }
-    if (isNaN(msg[2])) {
+    
+    if (isNaN(msg[2]) || isFinite(msg[2]) || msg[2] == Infinity || msg[2] == -Infinity || msg[2] == "Infinity") {
         cmsg.reply("Please specify the amount of topup");
         return;
     }
@@ -35,9 +36,15 @@ module.exports.run = async (msg, client, cmsg) => {
                     ex = snapshot.exists();
                     data = snapshot.val();
                     if (ex) {
-                        let coin = parseInt(msg[2]);
+                        let coin = parseInt(msg[2], 10);
+                        if (coin <= 0){
+                            cmsg.reply("Please specify the amount of topup");
+                            return;
+
+                        }
                         console.log(coin);
-                        let coinNow = data.coin + coin;
+                        let datc = parseInt(data.coin, 10);
+                        let coinNow = datc + coin;
                         database.ref('users_wa/' + num).update({ coin: coinNow });
     
                         client.sendMessage(num+"@c.us", "Successfully topup $ "+coin+"\nThanks good people!");
@@ -58,9 +65,16 @@ module.exports.run = async (msg, client, cmsg) => {
                     ex = snapshot.exists();
                     data = snapshot.val();
                     if (ex) {
-                        let coin = parseInt(msg[2]);
+                        let coin = parseInt(msg[2], 10);
+                        if (coin <= 0){
+                            cmsg.reply("Please specify the amount of topup");
+                            return;
+
+                        }
                         console.log(coin);
-                        let coinNow = data.coin + coin;
+                        let datc = parseInt(data.coin, 10);
+                        let coinNow = datc + coin;
+                        console.log(coinNow);
                         database.ref('users_wa/' + number).update({ coin: coinNow });
     
                          client.sendMessage(number+"@c.us", "Successfully topup $ "+coin+"\nThanks good people!");
